0a7b11e60baa3582cb09edfd4acebb23042bb192
John Stafford <john@LD001ITD-JSTA2.(none)>
2013-05-01 13:25:59 -0700

    Added Script

---
+++ getapod.sh
@@ -0,0 +1,10 @@
+cd S#!/bin/bash
+# Simple Script to download and load the NASA APOD photo as the wallpaper
+# Load as cronjob with 0 10 * * * DISPLAY=:0.0 /home/john/scripts/getapod.sh (note the DISPLAY=:0.0)
+
+DATE='/bin/date +%y%m%d' 
+wget http://antwrp.gsfc.nasa.gov/apod/ap`$DATE`.html -O- 2> /dev/null |
+grep -m 1 jpg | 
+sed -e 's/<//' -e 's/>//' -e 's/.*=//' -e 's/"//g' -e 's/^/http:\/\/antwrp.gsfc.nasa.gov\/apod\//' |
+wget -i- -O ~/images/wallpapers/apod-`$DATE`.jpg
+nitrogen --set-zoom-fill ~/images/wallpapers/apod-`$DATE`.jpg
